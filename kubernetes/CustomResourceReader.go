ea8e6ca4dd7517bc48bdc82a89a415e127e99ac4
deB4SH <git@b4sh.de>
2022-09-08 18:30:38 +0200

    add: CR reader

---
+++ kubernetes/CustomResourceReader.go
@@ -0,0 +1,95 @@
+package kubernetes
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+type CustomResource struct {
+	Api        string
+	ApiVersion string
+	Namespace  string
+
+	Resource string
+	Name     string
+}
+
+type CustomResourceData struct {
+	Name string
+	Data []string
+}
+
+type KubernetesConnection struct {
+	config *rest.Config
+}
+
+var isCluster = flag.Bool("is.cluster", false, "Boolean toggle to define if application is deployed inside a cluster or not.")
+
+func NewCustomResource(api string, apiversion string, namespace string, resource string, name string) *CustomResource {
+	e := new(CustomResource)
+	e.Api = api
+	e.ApiVersion = apiversion
+	e.Namespace = namespace
+	e.Resource = resource
+	e.Name = name
+	return e
+}
+
+func newKubernetesConnection(config *rest.Config) *KubernetesConnection {
+	e := new(KubernetesConnection)
+	e.config = config
+	return e
+}
+
+func ParseCR(c *CustomResource) {
+
+	var k8Config *KubernetesConnection
+
+	if *isCluster {
+		fmt.Print("Starting to use incluster config")
+		config, err := rest.InClusterConfig()
+		if err != nil {
+			fmt.Printf("error getting Kubernetes config: %v\n", err)
+			os.Exit(1)
+		}
+		k8Config = newKubernetesConnection(config)
+	} else {
+		userHomeDir, err := os.UserHomeDir()
+		if err != nil {
+			fmt.Printf("error getting user home dir: %v\n", err)
+			os.Exit(1)
+		}
+		kubeConfigPath := filepath.Join(userHomeDir, ".kube", "config")
+		fmt.Printf("Using kubeconfig: %s\n", kubeConfigPath)
+
+		config, err := clientcmd.BuildConfigFromFlags("", kubeConfigPath)
+		if err != nil {
+			fmt.Printf("error getting Kubernetes config: %v\n", err)
+			os.Exit(1)
+		}
+		k8Config = newKubernetesConnection(config)
+	}
+
+	fmt.Print("Creating clientset")
+	clientset, err := kubernetes.NewForConfig(k8Config.config)
+	if err != nil {
+		fmt.Printf("Could not aquire clientset from config: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Print("Query requested cr")
+	data, err := clientset.RESTClient().Get().AbsPath(fmt.Sprintf("/apis/%s/%s", c.Api, c.ApiVersion)).Namespace(c.Namespace).Resource(c.Resource).Name(c.Name).DoRaw(context.TODO())
+	if err != nil {
+		fmt.Printf("Could not find custom resource:\n %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Print(data)
+}
--- main.go
+++ main.go
@@ -2,41 +2,34 @@ package main
 
 import (
 	"flag"
-	"net/http"
 
-	pmt "github.com/deb4sh/PrometheusCustomResourceExporter/prometheus"
-	"github.com/prometheus/client_golang/prometheus/promhttp"
+	k8api "github.com/deb4sh/PrometheusCustomResourceExporter/kubernetes"
+	//"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 // variables to start the server
 var listenAddress = flag.String("web.listen-address", ":9888", "Address to listen on for web interface.")
 var metricPath = flag.String("web.metrics-path", "/metrics", "Path under which to expose metrics.")
 
-func serverMetrics(listenAddress, metricsPath string) error {
-	http.Handle(metricsPath, promhttp.Handler())
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(`
-			<html>
-			<head><title>Custom Resource Exporter</title></head>
-			<body>
-			<h1>Possible Endpoints</h1>
-			<p><a href='` + metricsPath + `'>Prometheus Metrics</a></p>
-			</body>
-			</html>
-		`))
-	})
-	return http.ListenAndServe(listenAddress, nil)
-}
+// func serverMetrics(listenAddress, metricsPath string) error {
+// 	http.Handle(metricsPath, promhttp.Handler())
+// 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+// 		w.Write([]byte(`
+// 			<html>
+// 			<head><title>Custom Resource Exporter</title></head>
+// 			<body>
+// 			<h1>Possible Endpoints</h1>
+// 			<p><a href='` + metricsPath + `'>Prometheus Metrics</a></p>
+// 			</body>
+// 			</html>
+// 		`))
+// 	})
+// 	return http.ListenAndServe(listenAddress, nil)
+// }
 
 // start the service
 func main() {
-	m := pmt.NewMetricsManager("metrics", 8080)
-	m.AddMetricPath("prometheus", promhttp.Handler())
-
-	m.Serve()
-
-	for {
-		// do something
-	}
-
+	//currently for debug purpses
+	cr := k8api.NewCustomResource("k3s.cattle.io", "v1", "kube-system", "addons", "ccm")
+	k8api.ParseCR(cr)
 }
